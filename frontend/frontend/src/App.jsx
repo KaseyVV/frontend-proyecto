ae93c1864fdd5449a78bc4a2508676b62f9ee1d2
KaseyVV <andradekasey10@gmail.com>
2025-10-09 22:15:40 -0500

    Estructura Frontend + React

---
+++ frontend/frontend/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>🎮 GameTracker</h1>
+      <p>Bienvenido a tu biblioteca de juegos</p>
+    </div>
+  );
+}
+
+export default App
